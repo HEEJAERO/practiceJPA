3002c9e6a981a6022eb15f564098006a3a68016d
heejaeRo <rhj0830@gmail.com>
2022-05-23 15:36:29 +0900

    5/22 스프링 데이터 JPA, QueryDsl 맛보기 및 주문 취소 누락 부분 추가

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,37 +1,12 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    //@PersistenceContext  스프링데이터JPA 가 autowired 도 앞의 어노테이션과 같은 역할을 하도록 해준다?
-    private final EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll(){ // JPQL 로 조회해야함  -> entity 를 대상으로 조회 , SQL은 DB를 대상으로..
-        //
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name",Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
+    // select m from Member m from m.name = :name 으로 자동 생성 -> name 변수에 해당하는 값 대입해서 가져옴
+    List<Member> findByName(String name);
 }
---
+++ src/main/java/jpabook/jpashop/repository/MemberRepositoryOld.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepositoryOld {
+
+    //@PersistenceContext  스프링데이터 JPA 가 autowired 도 앞의 어노테이션과 같은 역할을 하도록 해준다?
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){ // JPQL 로 조회해야함  -> entity 를 대상으로 조회 , SQL은 DB를 대상으로..
+        //
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name",Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
